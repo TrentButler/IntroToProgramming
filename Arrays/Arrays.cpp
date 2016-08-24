45de351a04cac443481a05a6348948e7a6b2ae9d
Trent Butler <redtrentbutler@gmail.com>
2016-08-24 17:53:29 -0500

    IntroToProgramming 08-24-16
    Arrays exercises 1-4

---
+++ Arrays/Arrays.cpp
@@ -0,0 +1,164 @@
+# include <iostream>
+# include <windows.h>
+using namespace std;
+int main()
+{
+	/*
+	std::cout << "Example of a Array \n";
+	system("pause");
+
+
+	int i = 0;
+	int num[10]={ 50,51,52,53,54,55,56,57,58,59 };
+
+	for (i = 50; i <= 60; i++)
+	{
+
+	std::cout << "\n" << num[10];
+	}
+	std::cout<<"\n";
+	system("pause");
+	*/
+
+
+	//EXERCISE 1
+	/*
+	std::cout << "Determine if the array is valid or invalid. \n";
+	std::cout << "\n";
+	std::cout << "int numbers[10] = {0,0,1,0,0,1,0,0,1,1}; \n";
+	std::cout << "-This array is valid. \n";
+	std::cout << "\n";
+	std::cout << "int matrix[5] = {1,2,3,4,5,6,7} \n";
+	std::cout << "-This array is invalid \n";
+	std::cout << "- Array's ammount is assigned '5' but is given more than 5 values. \n";
+	std::cout << "\n";
+	std::cout << "double radii[10] = (3.2, 4.7} \n";
+	std::cout << "This array is invalid. \n";
+	std::cout << "- Cannot contain a '(', it must be a scope operator. \n";
+	std::cout << "\n";
+	std::cout << "int table[7] = {2, , , 7, , 45, 39} \n";
+	std::cout << "- This array is invalid. \n";
+	std::cout << "- When using ',' a value must be placed. \n";
+	std::cout << "\n";
+	std::cout << "char codes[] = { 'A', 'X', '1', '2', 's'} \n";
+	std::cout << "- This array is valid. \n";
+	std::cout << "\n";
+	std::cout << "int blanks[]; \n";
+	std::cout << "- This array is valid. \n";
+	std::cout << "\n";
+	std::cout << "int collection[-20]; \n";
+	std::cout << "- This array is invalid. \n";
+	std::cout << "- Cannot have a negative ammont as ammount for array. \n";
+	std::cout << "\n";
+	std::cout << "int hours[3] = 8, 12, 16; \n";
+	std::cout << "- This array is invalid. \n";
+	std::cout << "- Must use scope operator. \n";
+	system("pause");
+	*/
+
+	//EXERCISE 2
+	/*
+
+	std::cout << "\n";
+	std::cout << "2. Given the following array definition: \n";
+	std::cout << "int values[] = {2,6,10,14}";
+	std::cout << "\n";
+	std::cout << "\n";
+	std::cout << "What does each of the following display? \n";
+	std::cout << "\n";
+	cout << "a) cout << values[2] \n";
+	cout << "b) cout<< ++values[0]; \n";
+	cout << "c) cout << values[1]++; \n";
+	cout << "d) x=2;   cout << values[++x]; \n";
+	cout << "e) cout << values[4]; \n";
+	std::cout << "\n";
+	std::cout << "\n";
+	cout << "a). 10 \n";
+	cout << "b). 3 \n";
+	cout << "c). 6 \n";
+	cout << "d). 14 \n";
+	cout << "e). -858993460 \n";
+	std::cout << "\n";
+	system("pause");
+	*/
+
+	//EXERCISE 3
+	/*
+	cout << "3. Write a for loop for (int data[10]) \n";
+	cout << "\n";
+	int a = 0;
+	int data[10]{ 10,9,8,7,6,5,4,3,2,1 };
+	for (a = 0; a < 10; a++)
+	{
+	cout << "\n" << data[a];
+
+	}
+	cout << "\n";
+	cout << "\n";
+	system("pause");
+	*/
+
+	//EXERCISE 4
+	/*
+	cout << "Create a program that asks for 5 numbers, display numbers in reverse order. \n";
+
+
+	int c = 0;
+	int b[5];
+	std::cin >> b[0];
+	std::cin >> b[1];
+	std::cin >> b[2];
+	std::cin >> b[3];
+	std::cin >> b[4];
+	for (c = 4; c >= 0; c--)
+	{
+	std::cout <<"\n" << b[c];
+
+	}
+
+
+
+	cout << "\n";
+	cout << "\n";
+	system("pause");
+	*/
+
+	//EXERCISE 5
+	
+int e[10];
+int largeNumber = e[0];
+int smallNumber = e[0];
+	int d;	
+	
+	cin >> e[0];
+	cin >> e[1];
+	cin >> e[2];
+	cin >> e[3];
+	cin >> e[4];
+	cin >> e[5];
+	cin >> e[6];
+	cin >> e[7];
+	cin >> e[8];
+	cin >> e[9];
+	for (d = 0; d < 10; d++)
+	
+		if (largeNumber < e[d])
+			largeNumber = e[d];
+			
+		if (smallNumber > e[d])
+			smallNumber = e[d];
+	
+
+		cout << largeNumber << smallNumber;
+
+		system("pause");
+
+
+
+
+
+
+
+		return 0;
+
+}
---
+++ Variables/Variables.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+	/*
+	//VARIABLES EXCERCISE
+	printf("Variables Excercise 08-18-16 \n");
+	printf("Trent Butler \n");
+	system("pause");
+	int a1 = 2 / 3 + 3 / 5;
+	int b1 = 2 % 3 + 3 % 5;
+	int c = 23 % 15 % -2;
+	int d = 25 * 1 / 2;
+	float e = 25 * 1.0 / 2;
+	int f = 25 * (1 / 2);
+	int g = 235 / 8 + 7;
+	float h = 235 / 8.0 + 7;
+	float i = ((20 + 1) / 2 - 2.0) / (23 + 3)*0.2;
+	//EXCERCISE 1
+	printf("1. Find the value of each onf the following expressions, or explain why it is not a valid expression. Try this without coding it first \n");
+	//example a)
+	printf("%i", a1);
+	printf(" \n");
+	//example b)
+	printf("%i", b1);
+	printf(" \n");
+	//example c)
+	printf("%i", c);
+	printf(" \n");
+	//example d)
+	printf("%i", d);
+	printf(" \n");
+	//example e)
+	printf("%i", e);
+	printf(" \n");
+	//example f)
+	printf("%i", f);
+	printf(" \n");
+	//example g)
+	printf("%i", g);
+	printf(" \n");
+	//example h)
+	printf("%i", h);
+	printf(" \n");
+	//example i)
+	printf("%i", i);
+	printf(" \n");
+	system("pause");
+	//EXCERCISE 2
+	printf("2. Select suitable variable names you might use for: \n");
+	printf("a). avgHeight \n");
+	printf("b). tax_rt>$65000 \n");
+	printf("c). tax_rt<$5000 \n");
+	printf("d). tot_cost_steamgame \n");
+	system("pause");
+	//EXCERCISE 3
+	printf("3. Select suitable data types to hold the following values: \n");
+	printf("a.) intenger \n");
+	printf("b.) float \n");
+	printf("c.) character \n");
+	printf("d.) boolean \n");
+	system("pause");
+	//EXCERCISE 4
+	printf("4. Create a program that swaps two numbers. \n");
+	system("pause");
+	int A = 22;
+	int B = 66;
+	printf("%i", A);
+	printf(" \n");
+	printf("%i", B);
+	printf(" \n");
+	system("pause");
+	int tmp = A;
+	A = B;
+	B = tmp;
+	printf("%i", A);
+	printf(" \n");
+	printf("%i", B);
+	printf(" \n");
+	system("pause");
+
+	//EXCERCISE 5
+	printf("5. Consider the following code segment. \n");
+	printf("a) What will be the output when the input is: \n");
+	system("pause");
+	//A)
+
+	printf("a) 1 A 45 \n");
+	printf("i:1 \n");
+	printf("ch: A \n");
+	printf("f: 45 \n");
+	system("pause");
+	//B)
+	printf("b) 1A45 \n");
+	printf("i: 1 \n");
+	printf("ch: A \n");
+	printf("f:45 \n");
+	//C)
+	system("pause");
+	printf("c) 1 9 45 \n");
+	printf("i: 1 \n");
+	printf("ch: 9 \n");
+	printf("f: 45 \n");
+	//D)
+	system("pause");
+	printf("d) 1 945 \n");
+	printf("i: 1 \n");
+	printf("ch: 9 \n");
+	printf("f: 45 \n");
+	//E
+	system("pause");
+	printf("e) B 45.6 \n");
+	printf("i: 1 \n");
+	printf("ch: A \n");
+	printf("f: 0 \n");
+	//F
+	system("pause");
+	printf("f) 1BC5.6 \n");
+	printf("i: 1 \n");
+	printf("ch:A \n");
+	printf("f: 0 \n");
+	system("pause");
+
+	//EXCERCISE 6
+	printf("6. Ask the user for 5 whole numbers. Output the average of these numbers. \n");
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+	int num5;
+	int average;
+	printf("Please enter 5 whole numbers \n");
+	std::cin >> num1 >> num2 >> num3 >> num4 >> num5;
+	average = (num1 + num2 + num3 + num4 + num5) / 5;
+	printf("%i", average, "\n");
+	system("pause");
+	*/
+	//EXCERCISE 7
+	std::cout << "This program will display the letter in the middle of two given letters. \n";
+	std::cout << " \n";
+	std::cout << "Please enter two letters. \n";
+	std::cout << " \n";
+	char lett1;
+	char lett2;
+	int display;
+	std::cin >> lett1 >> lett2;
+	display = ((lett1 + lett2) / 2);
+	std::cout << display;
+	
+	system("pause");
+	//Test
+}
