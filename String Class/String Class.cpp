ec1c1a0b807dae6d839f8500e128b54b9b361d22
Trent Butler <redtrentbutler@gmail.com>
2016-09-10 19:32:21 -0500

    IntroToProgramming 09-10-16

---
+++ String Class/String Class.cpp
@@ -0,0 +1,69 @@
+#include <windows.h>
+#include <iostream>
+#include "String Class Header.h"
+using namespace std;
+
+
+
+int main()
+{
+	char input = '0';
+	
+	
+	while (input != 'q')
+	{
+		cin >> input;
+
+		switch (input)
+		{
+		case '1': //String Length Exercise
+		{
+			stringLen();
+			break;
+		}
+
+		case '3': //String Compare Exercise
+		{
+			stringCompare();
+			break;
+		}
+
+		case '4': //String Append Exercise
+		{
+			stringAppend();
+			break;
+		}
+
+		case '5': //String Prepend Exercise
+		{
+			stringPrepend();
+			break;
+		} 
+		
+		
+		
+		
+		
+		
+		
+		case '10': //String Copy Example
+		{
+			stringCopy();
+			break;
+		}     
+
+
+	
+
+
+		case 'q':
+		{
+			break;
+		}
+		}
+
+
+	}
+
+
+}
